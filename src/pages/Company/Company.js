aaef3979bbd3506e88d009b3795e541764b54d6c
Yusen Jiang <yusenjia@buffalo.edu>
2022-03-04 16:55:46 -0800

    company page mobile complete

--- src/pages/Company/Company.js
+++ src/pages/Company/Company.js
@@ -1,8 +1,62 @@
 import React from "react";
-import "./Company.css"
+import "./Company.css";
+import img from "../../assets/co-img-1.png"
+import wave from "../../assets/blue-wave.svg"
+import Footer from "../../components/Footer/Footer"
+
 
 export default function Company(){
+
     return(
-        <h1>Company</h1>
+
+        <>
+
+
+        <div className="hero-section">
+            <div className="hero-title">
+                <h1>Our company focuses on <span>food and people.</span> </h1>
+            </div>
+        </div>
+
+        <div className="main-content">
+            <div className="content-section flex-container">
+                <div className="content-info">
+                    <h2 className="section-title">The home of fresh products</h2>
+                    <p className="section-para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente explicabo officiis quasi ipsum vel ipsam sequi eligendi eius at. Sed?</p>
+                    <button className="primary-btn">Learn about us</button>
+                </div>
+            </div>
+
+            <img src={img} alt="" />
+        </div>
+
+        <div className="static-content">
+            <div className="content-section">
+                <div>
+                    <h2 className="num">62</h2>
+                    <img src={wave} alt="" />
+                    <p>Fiesty Menu Items</p>
+                </div>
+                <div>
+                    <h2 className="num">139</h2>
+                    <img src={wave} alt="" />
+                    <p>Fiesty Menu Items</p>
+                </div>
+                <div>
+                    <h2 className="num">34</h2>
+                    <img src={wave} alt="" />
+                    <p>Fiesty Menu Items</p>
+                </div>
+                <div>
+                    <h2 className="num">57</h2>
+                    <img src={wave} alt="" />
+                    <p>Fiesty Menu Items</p>
+                </div>
+            </div>
+        </div>
+
+        <Footer/>
+       
+        </>
     )
 }
