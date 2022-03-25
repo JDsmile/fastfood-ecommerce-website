60481058ebf205b58913acd04c011597bfd37fb3
Yusen Jiang <yusenjia@buffalo.edu>
2022-03-24 19:18:36 -0700

    add alert to avoid duplicate items

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,23 +3,25 @@ import "./Menu.css";
 import data from "../../data/menu.json";
 import TextField from '@mui/material/TextField';
 import { MenuContext } from "../../Contexts/MenuContext";
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 
 
 export default function Menu(){
     const [menuTab,setMenuTab] = React.useState(data.Burgers)
-    const [activeBtn,setActiveBtn] = React.useState(false)
     const [tabIndex,setIndex] = React.useState(0)
     const menuList=["Burgers","Drinks","Sides"]
-
     const {CartItems,setCartItems} = React.useContext(MenuContext)
     const {productQuantity,setProductQuantity}= React.useContext(MenuContext)
-    const {itemIndex,setItemIndex}= React.useContext(MenuContext)
-
-    const textInput = React.useRef();
+    const [sucess,setSucess] = React.useState(undefined)
+    const [showMessage,setShowMessage] = React.useState(false)
+    const [index,setdIndex] = React.useState(0)
 
     const handleClick = (item,value) =>{
 
         setIndex(value)
+        setShowMessage(()=>false)
+        
         if(item && item==="Burgers"){
             setMenuTab(data.Burgers)
         } else if(item && item==="Drinks"){
@@ -30,13 +32,18 @@ export default function Menu(){
     }
 
     const sentToCart=(item,value)=>{
-
-        // setProductQuantity(prev=>textInput.current.value)
-        // setItemIndex(value)
+        
+        setdIndex(()=>value)
+        setShowMessage(()=>true)
         const currentItem = {"name":item.name,"price":item.price,"img":item.image,"quantity":productQuantity}
-        setCartItems(()=>[...CartItems,currentItem])   
-
-
+        if (CartItems.find(product => product.name === item.name)) {
+            setSucess((()=>false))
+            
+            return;
+          }
+          setCartItems([...CartItems, currentItem])
+          setSucess(()=>true)
+        //   setCheckDuplicate(()=>false)
     }
     
     return (
@@ -66,11 +73,20 @@ export default function Menu(){
                                 </div>
 
                                 <p className="item-description">{item.description}</p>
+
                                 
                                 <div className="sent-to-cart">          
                                     <button className="cart-btn" 
                                             onClick={()=>sentToCart(item,value)
                                             }>Add To Cart</button>
+
+                                    <Alert variant="outlined" severity={sucess? "success": "error"}
+                                    className="alert"
+                                    style={{visibility : showMessage && index===value ?"visible" : "hidden"}}>
+                                        
+                                        {sucess ? "Product added" :"Duplicate item"}
+                                            
+                                        </Alert>
                                 </div>
                             </div>
                         </div>
